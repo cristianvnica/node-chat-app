2f7af4ef7f87f549da18c18a5247ef23943df3d2
Cristian Nica <cristian.nica@live.com>
2018-06-21 20:58:57 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'jenifer',
-        text: 'Hey. I am Jennifer.'
-    });
 });
 
 socket.on('disconnect', function () {
