a11211e1d3daae2e3cdc87d86265d4d8753cb823
Cristian Nica <cristian.nica@live.com>
2018-06-21 20:44:51 +0300

    Added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected!');
 
+    socket.on('createMessage', (message) => {
+        socket.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: 123
+        });
+    });
+
     socket.on('disconnect', () => {
         console.log('A client disconnected');
     });
